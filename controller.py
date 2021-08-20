0702ceecb70e20b53dba402ff5a8e2bdb87df764
Giorgos Margaritis <gmargari@protonmail.com>
2021-08-20 16:16:24 +0300

    Reorganize code

--- controller.py
+++ controller.py
@@ -12,19 +12,14 @@ class Controller(object):
         self.model = model
         self.view = view
 
+        # If view is modified, call view_changed()
+        self.view.register(self.view_changed)
+
     def view_changed(self, *args, **kwargs) -> None:
         raise NotImplemented
 
     def run(self) -> None:
-        # If view is modified, call view_changed()
-        self.view.register(self.view_changed)
-
-        # Run view
         self.view.main_loop()
 
     def quit(self) -> None:
-        # Gracefully terminate each view
         self.view.quit()
-
-        # Terminate program
-        sys.exit(0)
